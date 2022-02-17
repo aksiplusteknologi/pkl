f1dd6d8adaa0374a2cc37b3f223746f2da7e6636
Antonio Saiful Islam <finallyantonio@gmail.com>
2022-02-18 00:56:31 +0700

    Vue templating

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const path = require('path');
 
 /*
  |--------------------------------------------------------------------------
@@ -13,4 +14,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
+    .alias({
+        '@': path.join(__dirname, 'resources/js')
+    })
     .sass('resources/sass/app.scss', 'public/css');
